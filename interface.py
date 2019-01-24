177794512f789c630169ad3b8f651c5cef9f7169
Rajesh <rajesingh@rbbn.com>
2019-01-25 01:32:52 +0530

    Integrated outlook into interface

--- interface.py
+++ interface.py
@@ -1,21 +1,62 @@
-from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QTableWidget, QHBoxLayout, QGridLayout, 
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QTableWidget, QHBoxLayout, QGridLayout,
 QTabWidget, QSizePolicy, QStyleFactory, QDialog, QTableWidgetItem)
 from tms_assigned import Tms_task
+from outlook import Outlook_task
 
 class SneakPeek(QDialog):
     def __init__(self, parent=None):
         super(SneakPeek, self).__init__(parent)
-        self.createTmsWidget()
         mainLayout = QHBoxLayout()
-        mainLayout.addWidget(self.createTmsWidget)
+        main_wid = QTabWidget()
+
+        tms_wid = QWidget()
+        outlook_wid = QWidget()
+
+        self.createTmsWidget()
+        self.createOutlookWidget()
+        tmsLayout = QHBoxLayout()
+        tmsLayout.addWidget(self.createTmsWidget)
+        tms_wid.setLayout(tmsLayout)
+        outlookLayout = QHBoxLayout()
+        outlookLayout.addWidget(self.createOutlookWidget)
+        outlook_wid.setLayout(outlookLayout)
+
+        main_wid.addTab(tms_wid, "TMS")
+        main_wid.addTab(outlook_wid, "OUTLOOK")
+        mainLayout.addWidget(main_wid)
         self.setLayout(mainLayout)
         self.setWindowTitle("Sneakpeek")
-        self.changeStyle('Fusion ')
+        self.changeStyle('Fusion')
 
     def changeStyle(self, styleName):
         QApplication.setStyle(QStyleFactory.create(styleName))
         
+    def createOutlookWidget(self):
 
+        olo = Outlook_task()
+        output_df = olo.getCalendarEntry()
+        print(output_df)
+        self.createOutlookWidget = QTabWidget()
+        
+        #self.sneakpeekWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
+        outlook = QWidget()
+        tabHeaderList = list(output_df.columns)
+        tabIndexList = list(output_df.index)
+        tableWidget = QTableWidget(len(tabIndexList), len(tabHeaderList))
+        #tableWidget = QTableWidget(10, 10)
+        
+        tableWidget.setHorizontalHeaderLabels(tabHeaderList)
+        for i in tabHeaderList:
+            for j in tabIndexList:
+                #print( output_df[0].loc[j,i])
+                tableWidget.setItem(j,tabHeaderList.index(i), QTableWidgetItem(output_df.loc[j,i]))
+        #print(output_df[0].loc[0, tabHeaderList[0]])
+        oulookhbox = QHBoxLayout()
+        oulookhbox.setContentsMargins(5, 5, 5, 5)
+        oulookhbox.addWidget(tableWidget)
+        outlook.setLayout(oulookhbox)
+        self.createOutlookWidget.addTab(outlook, 'Outlook events')
+        
     def createTmsWidget(self):
 
         tmo = Tms_task("rajesingh", "Myfri3nd5f@mi1y")
@@ -28,6 +69,7 @@ class SneakPeek(QDialog):
         self.createTmsWidget.addTab(tms_assigned, 'TMS Assigned by me')
         tms_assigned1 = self.createAssignedWidget(output_df[1])
         self.createTmsWidget.addTab(tms_assigned1, 'TMS Assigned to me')
+
     def createAssignedWidget(self, df):
         tms = QWidget()
         tabHeaderList = list(df.columns)
--- outlook.py
+++ outlook.py
@@ -12,44 +12,62 @@ from win32com.client import Dispatch
 from tabulate import tabulate
 import datetime ,timedelta
 import pdb
-def getCalendarEntry():
-    #OUTLOOK_FORMAT = '%m/%d/%Y %H:%M'
-    OUTLOOK_FORMAT = '%d/%m/%Y %H:%M'
-    outlook = Dispatch("Outlook.Application")
-    ns = outlook.GetNamespace("MAPI")
+import pandas as pd
+class Outlook_task:
+    def getCalendarEntry(self):
+        #OUTLOOK_FORMAT = '%m/%d/%Y %H:%M'
+        OUTLOOK_FORMAT = '%d/%m/%Y %H:%M'
+        outlook = Dispatch("Outlook.Application")
+        ns = outlook.GetNamespace("MAPI")
 
-    appointments = ns.GetDefaultFolder(9).Items
-    appointments.Sort("[Start]")
-    appointments.IncludeRecurrences = "True"
-    today = datetime.datetime.today()
-    begin = today.date().strftime("%m/%d/%Y")
-    tomorrow= datetime.timedelta( days= 7)+today
-    print (tomorrow)
-    end = tomorrow.date().strftime("%m/%d/%Y")
-    appointments = appointments.Restrict("[Start] >= '" +begin+ "' AND [END] <= '" +end+ "'")
-    events={'Start':[],'Subject':[],'Duration':[]}
-    calcTableHeader = ['Title', 'Organizer', 'Start', 'Duration(Minutes)'];
-    calcTableBody = [];
-    for appointmentItem in appointments:
-    # adate=datetime.datetime.fromtimestamp(int(a.Start))
-        #print a.Start, a.Subject,a.Duration
-    #    events['Start'].append(a.date)
-    #   events['Subject'].append(a.Subject)
-    #  events['Duration'].append(a.Duration)
-        row = []
-        row.append(appointmentItem.Subject)
-        row.append(appointmentItem.Organizer)
-        row.append(appointmentItem.Start.Format(OUTLOOK_FORMAT))
-        row.append(appointmentItem.Duration)
-        calcTableBody.append(row)
-        #calcTableBody.append(events)
-        #  print (calcTableBody)
-    return  (tabulate(calcTableBody, headers=calcTableHeader));    
-    #return calcTableBody
+        appointments = ns.GetDefaultFolder(9).Items
+        appointments.Sort("[Start]")
+        appointments.IncludeRecurrences = "True"
+        today = datetime.datetime.today()
+        begin = today.date().strftime("%m/%d/%Y")
+        tomorrow= datetime.timedelta( days= 100)+today
+        #print (tomorrow)
+        end = tomorrow.date().strftime("%m/%d/%Y")
+        appointments = appointments.Restrict("[Start] >= '" +begin+ "' AND [END] <= '" +end+ "'")
+        events={'Start':[],'Subject':[],'Duration':[]}
+        keys = ('Title', 'Organizer', 'Start', 'Duration(Minutes)')
+        #print (type(keys))
+        calcTable = dict.fromkeys(keys)
+        #print(calcTable)
+        for appointmentItem in appointments:
+            if calcTable['Title']==None:
+                calcTable['Title'] = appointmentItem.Subject
+                calcTable['Organizer'] = appointmentItem.Organizer
+                calcTable['Start'] = appointmentItem.Start.Format(OUTLOOK_FORMAT)
+                calcTable['Duration(Minutes)'] = appointmentItem.Duration
+            elif type(calcTable['Title']) == list:
+                calcTable['Title'].append(appointmentItem.Subject)
+                calcTable['Organizer'].append(appointmentItem.Organizer)
+                calcTable['Start'].append(appointmentItem.Start.Format(OUTLOOK_FORMAT))
+                calcTable['Duration(Minutes)'].append(appointmentItem.Duration)
+            else:
+                calcTable['Title'] = [calcTable['Title'],appointmentItem.Subject]
+                calcTable['Organizer'] = [calcTable['Organizer'],appointmentItem.Organizer]
+                calcTable['Start'] = [calcTable['Start'], appointmentItem.Start.Format(OUTLOOK_FORMAT)]
+                calcTable['Duration(Minutes)'] = [calcTable['Duration(Minutes)'], appointmentItem.Duration]
+            
+            
+            #calcTableBody.append(events)
+            #  print (calcTableBody)
+        #print(calcTableHeader)
+        #print (calcTableBody)
+        #print (dict(zip(calcTableHeader,calcTableBody) ) )
+        #print (calcTable)
+        dfs = pd.DataFrame.from_dict(calcTable)
+        print(dfs)
+        return dfs
+        #return  (tabulate(calcTableBody, headers=calcTableHeader));   
+        
+        #return calcTableBody
 
 
-def GetData():
-    data =getCalendarEntry()
-    print (data)
+# def GetData():
+#     getCalendarEntry()
+#     #print (type(data))
     
-GetData()
+# GetData()
