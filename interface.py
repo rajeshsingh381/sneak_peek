6349e206614c75807db27d9dfe039df683b9419c
Rajesh <rajesingh@rbbn.com>
2019-01-24 22:20:18 +0530

    added common function for tms tabwidget

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QTableWidget, QHBoxLayout, QGridLayout, QTabWidget, QSizePolicy, QStyleFactory, QDialog
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QTableWidget, QHBoxLayout, QGridLayout, 
+QTabWidget, QSizePolicy, QStyleFactory, QDialog, QTableWidgetItem)
+from tms_assigned import Tms_task
 
 class SneakPeek(QDialog):
     def __init__(self, parent=None):
@@ -15,16 +17,36 @@ class SneakPeek(QDialog):
         
 
     def createTmsWidget(self):
-        self.createTmsWidget = QTabWidget()
-        #self.sneakpeekWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
-        tms = QWidget()
-        tableWidget = QTableWidget(10, 10)
 
+        tmo = Tms_task("rajesingh", "Myfri3nd5f@mi1y")
+        output_df = tmo.assigned_func()
+        print(output_df)
+        self.createTmsWidget = QTabWidget()
+        
+        #self.sneakpeekWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Ignored)
+        tms_assigned = self.createAssignedWidget(output_df[0])
+        self.createTmsWidget.addTab(tms_assigned, 'TMS Assigned by me')
+        tms_assigned1 = self.createAssignedWidget(output_df[1])
+        self.createTmsWidget.addTab(tms_assigned1, 'TMS Assigned to me')
+    def createAssignedWidget(self, df):
+        tms = QWidget()
+        tabHeaderList = list(df.columns)
+        tabIndexList = list(df.index)
+        tableWidget = QTableWidget(len(tabIndexList), len(tabHeaderList))
+        #tableWidget = QTableWidget(10, 10)
+        
+        tableWidget.setHorizontalHeaderLabels(tabHeaderList)
+        for i in tabHeaderList:
+            for j in tabIndexList:
+                #print( output_df[0].loc[j,i])
+                tableWidget.setItem(j,tabHeaderList.index(i), QTableWidgetItem(df.loc[j,i]))
+        #print(output_df[0].loc[0, tabHeaderList[0]])
         tmshbox = QHBoxLayout()
         tmshbox.setContentsMargins(5, 5, 5, 5)
         tmshbox.addWidget(tableWidget)
         tms.setLayout(tmshbox)
-        self.createTmsWidget.addTab(tms, 'TMS Assigned to me')
+        return tms
+
 
 
 if __name__ == '__main__':
